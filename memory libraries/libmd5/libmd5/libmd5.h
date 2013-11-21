f869084ba8ac8cd1fcb1593d70e84378ea091abb
Bas Groothedde <bas@imagine-programming.com>
2013-11-21 01:54:12 +0100

    licenses

--- memory libraries/libmd5/libmd5/libmd5.h
+++ memory libraries/libmd5/libmd5/libmd5.h
@@ -1,5 +1,54 @@
 #include "stdafx.h"
 
+/*
+License 1:            MIT
+[=[
+Copyright (c) 2013 Imagine Programming, Bas Groothedde
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+]=]
+
+License 2:          RSA MD5 message-digest algorithm
+[=[
+MD5 - RSA Data Security, Inc., MD5 message-digest algorithm
+Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All rights reserved.
+
+License To copy And use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+Or this function.
+
+License is also granted To make And use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning Or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software Or the suitability of this
+software For any particular purpose. It is provided "as is"
+without express Or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this
+documentation and/Or software.
+]=]
+*/
+
 #ifndef libmd5_h
 	#define libmd5_h
 
